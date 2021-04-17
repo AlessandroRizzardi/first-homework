cc872e84198be00561a64f2469b704ca15985764
AlessandroRizzardi <alessandro.rizzardi@studenti.unitn.it>
2021-04-17 19:02:25 +0200

    added include and src, modified svg drawing

---
+++ include/craine.h
@@ -0,0 +1,55 @@
+#ifndef CRAINE_H
+#define CRAINE_H
+
+
+struct Craine{
+
+    double base_width;
+    double base_height;
+
+    double sliding;
+
+    double base;
+    double height;
+
+    double arm;
+
+    double angle;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
